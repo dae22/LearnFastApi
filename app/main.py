832acb410b007c638f99ede3f541ea6ba98d4d9f
dae22 <dae-22@mail.ru>
2025-03-04 11:41:11 +0300

    Commit 04/03

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from models.models import Feedback
+from fastapi import FastAPI, Cookie, Response, HTTPException
+from uuid import uuid1
+from models.models import User
 
 
 app = FastAPI()
 
-fake_db = []
+fake_db: list[User] = [User(username='user123', password='password123'),
+                       User(username="dae22", password="yanedyrak"),
+                       User(username="Noigu", password="hoyopo")]
+sessions: dict = {}
 
-@app.post('/feedback')
-def add_feedback(feedback: Feedback):
-    fake_db.append(feedback)
-    return {"message": "Feedback added"}
+
+@app.get('/login')
+async def login(user: User, response: Response):
+    for person in fake_db:
+        if person.username == user.username and person.password == user.password:
+            return {"message": "You got my secret, welcome"}
+        raise HTTPException(status_code=401)
+
+
+
+
+
