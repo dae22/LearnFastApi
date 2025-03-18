ae7055c32ee27f718ec0f5cb274d49277452fc0d
dae22 <dae-22@mail.ru>
2025-03-18 16:55:07 +0300

    finish CRUD lesson

--- app/main.py
+++ app/main.py
@@ -40,3 +40,26 @@ async def get_user(user_id: int):
         return UserReturn(username=result["username"], email=result["email"], id=user_id)
     else:
         raise HTTPException(status_code=404, detail="User not found")
+
+@app.put("/users/{user_id}", response_model=UserReturn)
+async def update_user(user_id: int, user:UserCreate):
+    query = "UPDATE users SET username=:username, email=:email WHERE id=:id"
+    values = {"username": user.username, "email": user.email, "id": user_id}
+    try:
+        await database.execute(query=query, values=values)
+        return {**user.dict(), "id": user_id}
+    except:
+        raise HTTPException(status_code=500, detail="Failed to update user in database")
+
+@app.delete("/users/{user_id}", response_model=dict)
+async def delete_user(user_id: int):
+    query = "DELETE FROM users WHERE id=:id RETURNING id"
+    values = {"id": user_id}
+    try:
+        deleted_rows = await database.execute(query=query, values=values)
+    except:
+        raise HTTPException(status_code=500, detail="Failed to delete user from database")
+    if deleted_rows:
+        return {"message": " User deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
