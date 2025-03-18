da37caabbe42137ce6d82a4394ffb11bc7982b15
dae22 <dae-22@mail.ru>
2025-03-18 16:27:29 +0300

    postgresql conn 2

---
+++ Example/FastAPI+SQLite/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+DB_NAME = ("database.sqlite")
+
+conn = sqlite3.connect(DB_NAME)
+cursor = conn.cursor()
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    completed TEXT NOT NULL
+)
+""")
+
+conn.commit()
+conn.close()
