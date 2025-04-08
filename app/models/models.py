8506a51bc76cd2cb909961ec98c945c39e3477ea
dae22 <dae-22@mail.ru>
2025-04-08 17:10:41 +0300

    Finish Todo with PostgreSQL

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+class Base(DeclarativeBase):
+    pass
 
-class Todo(BaseModel):
-    title: str
-    description: str
-    completed: str = 'False'
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
